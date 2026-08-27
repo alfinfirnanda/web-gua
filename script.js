54ab78f9785cb6a9a4bfb5ca6e4d18dd8b56e1f9
alfinfirnanda <alfin.firnanda@siswa.smp.ibb.sch.id>
2026-08-27 10:54:39 +0800

    pesan perubahan kamu

--- script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll(".option-btn");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Kamu memilih " + button.textContent);
-    });
-});
